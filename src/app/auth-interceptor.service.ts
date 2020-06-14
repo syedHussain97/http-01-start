1bc675c2cd3cbe5b7ede46abe3305527de07f82b
Syed Hussain <syed.m.husyn@gmail.com>
2020-06-15 08:54:28 +1200

    adding logging interceptor service

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -9,10 +9,10 @@ export class AuthInterceptorService implements HttpInterceptor {
     console.log(req.url);
     const requestClone = req.clone({headers: req.headers.append('Auth', 'xyz')});
     return next.handle(requestClone).pipe(tap(event => {
-      console.log(event);
+      // console.log(event);
       if (event.type === HttpEventType.Response) {
-        console.log('Response agaya maidan mei, body data:');
-        console.log(event.body);
+        // console.log('Response agaya maidan mei, body data:');
+        // console.log(event.body);
       }
     }));
   }
